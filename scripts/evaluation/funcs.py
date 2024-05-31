ab41790b10273f96c58e436db9a7e0a9e8cac0c1
kijai <40791699+kijai@users.noreply.github.com>
2024-05-31 03:01:47 +0300

    Update funcs.py

--- scripts/evaluation/funcs.py
+++ scripts/evaluation/funcs.py
@@ -3,33 +3,39 @@ from collections import OrderedDict
 import torch
 #sys.path.insert(1, os.path.join(sys.path[0], '..', '..'))
 from einops import rearrange
-
+from safetensors.torch import load_file
 
 def load_model_checkpoint(model, ckpt):
     def load_checkpoint(model, ckpt, full_strict):
-        state_dict = torch.load(ckpt, map_location="cpu")
+        if "safetensors" in ckpt:
+            try:
+                state_dict = load_file(ckpt)
+            except:
+                state_dict = torch.load(ckpt, map_location="cpu")
+        else:
+            state_dict = torch.load(ckpt, map_location="cpu")
         if "state_dict" in list(state_dict.keys()):
             state_dict = state_dict["state_dict"]
-            try:
-                model.load_state_dict(state_dict, strict=full_strict)
-            except:
-                ## rename the keys for 256x256 model
-                new_pl_sd = OrderedDict()
-                for k,v in state_dict.items():
-                    new_pl_sd[k] = v
-
-                for k in list(new_pl_sd.keys()):
-                    if "framestride_embed" in k:
-                        new_key = k.replace("framestride_embed", "fps_embedding")
-                        new_pl_sd[new_key] = new_pl_sd[k]
-                        del new_pl_sd[k]
-                model.load_state_dict(new_pl_sd, strict=full_strict)
-        else:
-            ## deepspeed
+        try:
+            model.load_state_dict(state_dict, strict=full_strict)
+        except:
+            ## rename the keys for 256x256 model
             new_pl_sd = OrderedDict()
-            for key in state_dict['module'].keys():
-                new_pl_sd[key[16:]]=state_dict['module'][key]
+            for k,v in state_dict.items():
+                new_pl_sd[k] = v
+
+            for k in list(new_pl_sd.keys()):
+                if "framestride_embed" in k:
+                    new_key = k.replace("framestride_embed", "fps_embedding")
+                    new_pl_sd[new_key] = new_pl_sd[k]
+                    del new_pl_sd[k]
             model.load_state_dict(new_pl_sd, strict=full_strict)
+        # else:
+        #     ## deepspeed
+        #     new_pl_sd = OrderedDict()
+        #     for key in state_dict['module'].keys():
+        #         new_pl_sd[key[16:]]=state_dict['module'][key]
+        #     model.load_state_dict(new_pl_sd, strict=full_strict)
 
         return model
     load_checkpoint(model, ckpt, full_strict=True)
